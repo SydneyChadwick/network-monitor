b59346344b13660e1849221d138af80ed2b1a9ca
Sydney Chadwick <sydney@afrigis.co.za>
2016-05-15 12:27:45 +0200

    New project

---
+++ src/main/java/co/za/pseudos/NetworkMonitorApplication.java
@@ -0,0 +1,12 @@
+package co.za.pseudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkMonitorApplication.class, args);
+	}
+}
