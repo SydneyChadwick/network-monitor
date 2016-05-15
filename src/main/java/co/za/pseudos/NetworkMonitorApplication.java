823ad39b141fd61fc4b5a74686e50269fe215c6a
Sydney Chadwick <sydney@afrigis.co.za>
2016-05-15 13:19:06 +0200

    Added findbugs, pmd, checkstyle, logback

--- src/main/java/co/za/pseudos/NetworkMonitorApplication.java
+++ src/main/java/co/za/pseudos/NetworkMonitorApplication.java
@@ -3,10 +3,26 @@ package co.za.pseudos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Run the application.
+ * 
+ * @author Sydney
+ *
+ */
 @SpringBootApplication
 public class NetworkMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetworkMonitorApplication.class, args);
-	}
+    /**
+     * Empty constructor
+     */
+    public NetworkMonitorApplication() {
+    }
+
+    /**
+     * Run application.
+     * @param args Application args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkMonitorApplication.class, args);
+    }
 }
